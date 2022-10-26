46f038a03b509d2a5d512be630a90ab63178ee23
Cinthia Sanchez Gallardo <ceenti@MacBook-Pro-de-Cinthia.local>
2022-10-25 21:22:43 -0500

    [config] chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import RoutesIndex from './routes'
+import Navbar from './pages/NavBar'
+
+function App () {
+  return (
+    <div className='App'>
+      <Navbar />
+      <RoutesIndex />
+    </div>
+  )
+}
+
+export default App
---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+
+const RoutesIndex = () => {
+  <Routes>
+    <Route path='/' element={<Home />} />
+  </Routes>
+}
+export default RoutesIndex
